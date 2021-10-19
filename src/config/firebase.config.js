759c9d3e5c3dadc55f565e0d6bc96be53a5aa7d5
DamanB <damanbharaj@gmail.com>
2021-10-18 22:20:17 -0400

    fixed mobile bugs

---
+++ src/config/firebase.config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDpQPS758h6HTNgmh32vkOGf7yjan3KTz0",
+  authDomain: "boiler-luxurybarber.firebaseapp.com",
+  projectId: "boiler-luxurybarber",
+  storageBucket: "boiler-luxurybarber.appspot.com",
+  messagingSenderId: "462992205693",
+  appId: "1:462992205693:web:160312f0f57bb17330ce14"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
